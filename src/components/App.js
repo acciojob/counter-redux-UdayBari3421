4599099a275477c41fbc53e5e47b7fdb136c7476
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-03-07 14:47:35 +0000

    4rth

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./../styles/App.css";
 
 const App = () => {
-  const [count, setCount] = useState(2);
+  const [count, setCount] = useState(0);
   return (
     <div
       style={{
