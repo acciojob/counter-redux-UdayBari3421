455148d4d2bdb91bcad154f1fb9851fafc051a13
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-03-07 14:41:24 +0000

    2nd

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,16 @@ import "./../styles/App.css";
 const App = () => {
   const [count, setCount] = useState(0);
   return (
-    <div style={{ display: "flex", gap: "15px", height: "20px" }}>
+    <div
+      style={{
+        display: "flex",
+        gap: "15px",
+        height: "20px",
+        alignItems: "center",
+      }}
+    >
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <p style={{ margin: 0 }}>{count}</p>
+      <h1 style={{ margin: 0 }}>{count}</h1>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
