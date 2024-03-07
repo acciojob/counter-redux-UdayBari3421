35ebfff91705bb0e1e720bfb567c8a66e18e2255
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-03-07 14:38:10 +0000

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{ display: "flex", gap: "15px", height: "20px" }}>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <p style={{ margin: 0 }}>{count}</p>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
